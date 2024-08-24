b58668b37b24cbc2c6f9a0f2affaf3f3b0c84d86
alie20 <alieldeen81@gmail.com>
2024-08-24 23:58:40 +0300

    update task2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -34,7 +34,7 @@ rm -rf "${WRITEDIR}"
 # create $WRITEDIR if not assignment1
 assignment=`cat ../conf/assignment.txt`
 
-if [ $assignment = 'assignment2' ]
+if [ $assignment != 'assignment1' ]
 then
 	mkdir -p "$WRITEDIR"
 
@@ -48,8 +48,8 @@ then
 		exit 1
 	fi
 fi
-echo "Removing the old writer utility and compiling as a native application"
-#make clean
+#echo "Removing the old writer utility and compiling as a native application"
+make clean
 make
 
 for i in $( seq 1 $NUMFILES)
@@ -60,7 +60,7 @@ done
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
-#rm -rf /tmp/aeld-data
+rm -rf /tmp/aeld-data
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
